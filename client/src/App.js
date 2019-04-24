5aa3a06d09ca8f94fd9899ff434b0a787a60a38d
Sy Lim <slimdoe2@gmail.com>
2019-04-24 11:12:01 -0700

    set up templates

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
